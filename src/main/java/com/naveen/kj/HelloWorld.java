305c20c06670e41ecd3fad8dec4523c451f7fb4e
njain7242 <naveen.kj@flipkart.com>
2015-07-25 20:49:49 +0530

    First Commit

---
+++ src/main/java/com/naveen/kj/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.naveen.kj;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
