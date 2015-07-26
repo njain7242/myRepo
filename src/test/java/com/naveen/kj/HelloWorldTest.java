63e1ab9198907d114a2544c932a3a07f96f79ba6
njain7242 <naveen.kj@flipkart.com>
2015-07-26 19:02:28 +0530

    bugFix

--- src/test/java/com/naveen/kj/HelloWorldTest.java
+++ src/test/java/com/naveen/kj/HelloWorldTest.java
@@ -3,11 +3,10 @@ package com.naveen.kj;
 
 import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-<<<<<<< HEAD
 public class HelloWorldTest
 {
-        System.out.println("Hello World Test");
+        @Test
+        public void testApp(){
+                System.out.println("Hello World Test");
+        }
 }
